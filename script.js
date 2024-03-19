5c563bf1d645b5b47e53b97f8fc71fb97622db06
Brandon Blackmoor <bblackmoor@blackgate.net>
2024-03-19 18:07:29 -0400

    Initial commit.

---
+++ script.js
@@ -0,0 +1,297 @@
+let data = new Object();
+let front = false;
+let i = 0;
+
+// Getting the front and the back titles
+const titles = document.querySelectorAll(".title");
+
+// Getting the front and the back text boxes
+const texts = document.querySelectorAll(".text");
+
+// Getting the body
+const body = document.getElementById("body");
+
+// Getting the buttons
+const button = document.querySelectorAll(".new-card");
+
+const blockFront = document.querySelector(".block__front");
+const blockBack = document.querySelector(".block__back");
+
+const titleFront = titles[0];
+const titleBack = titles[1];
+
+const textFront = texts[0];
+const textBack = texts[1];
+
+const buttonFront = button[0];
+const buttonBack = button[1];
+
+let copyText = "";
+
+const cardIcon = "<!-- Icon -->\n<i class=\"fa-solid fa-flip-horizontal fa-scale-unbalanced\"></i>\n";
+const copySpanStart = "<span class=\"copy-span\" onclick=\"copyCard()\">\n";
+const copySpanEnd = "</span>\n";
+const copyIcon = "<i class=\"fa-regular fa-copy\"></i>";
+
+// An arrow function used to get a card randomly
+const displayCard = () =>{
+	// Generates a random number between 0
+	// and the length of the dataset
+	let index = Math.floor(Math.random()*dataLength);
+
+	// Stores the title of the respective card
+	let cardTitle = data[index][0];
+
+	// Stores the text of the respective card
+	let cardText = data[index][1];
+
+	// The text if no text is present
+	if(!cardText) {
+		cardText = "Whimsy Cards copyright &copy; 1987 (Used without permission)"
+	}
+
+	// Replacing the current card and the text with a new one
+	if (front) {
+		// Changing the front if back-side is displayed
+		titleFront.innerHTML = copySpanStart + cardTitle + cardIcon + copySpanEnd;
+		textFront.innerHTML = copySpanStart + cardText + " " + copySpanEnd;
+	} else {
+		// Changing the back if front-side is displayed
+		titleBack.innerHTML = copySpanStart + cardTitle + cardIcon + copySpanEnd;
+		textBack.innerHTML = copySpanStart + cardText + " " + copySpanEnd;
+	}
+
+	copyText = cardTitle + "\n" + cardText;
+	
+	front = !front;
+}
+
+// Adding an onclick listener for the button
+function newCard() {
+	// Rotating the Card Box
+	blockBack.classList.toggle('rotateB');
+	blockFront.classList.toggle('rotateF');
+
+	// Displaying a new card when the page loads
+	displayCard();
+}
+
+// Adding an onclick listener to copy the card
+function copyCard() {
+	// Get the text field
+	//var copyText = titleFront.innerHTML;
+
+	// Select the text field
+	//copyText.select();
+	//copyText.setSelectionRange(0, 99999); // For mobile devices
+
+	// Copy the text inside the text field
+	//navigator.clipboard.writeText(copyText.value);
+	navigator.clipboard.writeText(copyText);
+}
+
+window.onload = function() {
+	data[i++] = [
+			"Abrupt Change Of Events",
+			"Suddenly things are not happening the way they were a moment ago. Alliances switch, secrets are revealed, and new information surfaces."
+	];
+	data[i++] = [
+			"Added Animosity",
+			"The ill-will between characters grows past current levels. This animosity can be openly expressed or harbored secretly in the heart."
+	];
+	data[i++] = [
+			"Bad Tidings",
+			"Someone gets bad news. It might affect play or it might be news of distant and still important events."
+	];
+	data[i++] = [
+			"Bizarre Coincidence",
+			"Two or more things come together against incredible odds. Old friends (and enemies) run into you in the supermarket, you just happen to have the rare item you need in your closet, etc."
+	];
+	data[i++] = [
+			"Change Of Heart",
+			"A character's feelings change and alter a decision. Pirates spare prisoners and hassled innkeepers decide to make room for you after all."
+	];
+	data[i++] = [
+			"Double Jeopardy",
+			"Failure carries a double penalty. It could mean the normal penalty doubled, but it could also mean a penalty in addition to the normal one."
+	];
+	data[i++] = [
+			"Draw a Blank",
+			"_"
+	];
+	data[i++] = [
+			"Emotional Release",
+			"Strong emotions are let out. What are the emotions, and what triggers their sudden eruption? Is it a healthy release or a wild tempest?"
+	];
+	data[i++] = [
+			"Envy",
+			"Someone covets something that another person has. The envy could be expressed as an outright attempt to get the object of desire or something more subtle."
+	];
+	data[i++] = [
+			"Erratic Behavior",
+			"Someone or something exhibits wild, unexpected behavior. Could be party members, enemies, mounts, spells, or equipment."
+	];
+	data[i++] = [
+			"Error Of Judgment",
+			"Someone's better judgment fails. The trouble that ensues depends on the specific error that is made."
+	];
+	data[i++] = [
+			"Extreme Caution",
+			"Someone exercises extreme caution. Is the caution necessary, or is it exaggerated? What is the disadvantage from using so much caution?"
+	];
+	data[i++] = [
+			"Greed",
+			"Desire for wealth diverts a character from normal activity. The object of this desire can be specific (e.g. a certain necklace) or general (e.g. gold!)."
+	];
+	data[i++] = [
+			"Horrible Failure",
+			"What was a simple failure becomes a disaster. Attempts to persuade people backfire, arrows strike the wrong targets, and the wrong demons get conjured."
+	];
+	data[i++] = [
+			"Inopportune Arrival",
+			"Someone or something shows up to the chagrin or disadvantage of someone. Unwelcome relatives and hated enemies are possibilities."
+	];
+	data[i++] = [
+			"Internal Conflict",
+			"Conflicts arise within a person or within a group. How did these conflicts come about? How can they be resolved? Are they hidden? Poorly hidden?"
+	];
+	data[i++] = [
+			"Jealousy",
+			"The green-eyed monster rears its ugly head. How does the jealousy show itself?"
+	];
+	data[i++] = [
+			"Joy",
+			"Delight floods a character, making the world seem beautiful. What brings about this wonderful feeling?"
+	];
+	data[i++] = [
+			"Lasting Impression",
+			"An impression is made, or an old one is reborn. Emotional experiences return to a character affecting actions or something happens that will leave an impression for years (e.g. a scar)."
+	];
+	data[i++] = [
+			"Malice",
+			"Wrath rises in a character toward someone or something. Even if the anger can be held in check, it will affect the person's behavior."
+	];
+	data[i++] = [
+			"Misguided Love",
+			"Love affects a character in ways others fail to approve of. The character seems out of control. (To the lover of course, the love is true and wonderful; to others, it is misguided.)"
+	];
+	data[i++] = [
+			"Misplaced Trust",
+			"Undeserved trust is or has been placed in someone or something. \"Faithful\" hirelings desert, \"sturdy\" ropes break, and politicians are believed."
+	];
+	data[i++] = [
+			"Moral Dilemma",
+			"A character is faced with a moral question. What should the character do? What are the personality traits or personal values that make the decision to difficult?"
+	];
+	data[i++] = [
+			"New Role",
+			"You take over another character, either of a player or of the gamemaster. The original owner can ask for it back and veto your actions while you have the new role."
+	];
+	data[i++] = [
+			"Ominous Omen",
+			"Something suggests future evil, an event that portends calamity, misfortune, or death. The players might not understand the significance of the omen."
+	];
+	data[i++] = [
+			"Parting Of Ways",
+			"People or things go their separate ways. The parting can be happy or sorrowful, anticipated or sudden. Depending on the circumstances, those who part might meet again."
+	];
+	data[i++] = [
+			"Personality Clash",
+			"The personalities of two characters come into conflict. The cause of the clash and the gravity are up to you. How could the conflict be resolved?"
+	];
+	data[i++] = [
+			"Pity",
+			"Sympathetic sorrow for another person affects a character's actions. This pity might provoke action or merely set a mood."
+	];
+	data[i++] = [
+			"Pivotal Decision",
+			"Someone is faced with a decision that will affect something dramatically. A potential ally chooses sides, or a leader chooses a plan of action."
+	];
+	data[i++] = [
+			"Second Chance",
+			"Someone gets a second chance. Maybe that bullet missed the heart after all, or maybe the character realizes something that allows a second skill attempt. You must give the reason."
+	];
+	data[i++] = [
+			"Sloth",
+			"Someone is very lazy. Guards become lax, squires neglect their duties, and bartenders give you slow service."
+	];
+	data[i++] = [
+			"Something Missing",
+			"Something that should be here is not. The seriousness of the lack depends on what is missing. It could be your sword or a friend you were supposed to meet."
+	];
+	data[i++] = [
+			"Special Circumstances",
+			"Unusual conditions change the normal course of events for good or ill. The circumstances can be personal, such as character's mood, impersonal, such as the weather."
+	];
+	data[i++] = [
+			"Spectacular Success",
+			"What was a normal success becomes spectacular. Those rare, unexplainable feats are now possible. The gamemaster must watch this card because it is easy to abuse."
+	];
+	data[i++] = [
+			"Sudden Reversion",
+			"Suddenly someone or something reverts to the way it used to be. People revert to earlier patterns of behavior and problems go back to the way they were at the beginning."
+	];
+	data[i++] = [
+			"Tables Turn",
+			"Something in the situation reverses, an advantage turns in to a disadvantage, a disadvantage turns into an advantage. It is a surprise to all."
+	];
+	data[i++] = [
+			"Things Are Not As They Seem",
+			"Characters have been deceived, perhaps without malicious intent. The truth need not be known now. A good card for a secret note to the GM."
+	];
+	data[i++] = [
+			"Trade Places",
+			"You take over the role of another character and the player who was running that character takes yours. Either of you can end the trade at any time."
+	];
+	data[i++] = [
+			"Turn For The Worse",
+			"Things were going well, but suddenly something comes up that changes things for the worse. What is the change? How can those affected overcome the new problem?"
+	];
+	data[i++] = [
+			"Ulterior Motive",
+			"A character has motives besides those that are already known, and the ulterior motive can conflict with the surface motive. A good Whimsy for a secret note to the gamemaster."
+	];
+	data[i++] = [
+			"Unexpected Aid",
+			"Much to your surprise, aid shows up. Aid could be anything from fog that helps you sneak into an enemy camp to the arrival of the cavalry. But does this aid have a price?"
+	];
+	data[i++] = [
+			"Unexplained Consequence",
+			"Someone's actions have results that were not, perhaps could not have been, foreseen. The result can be good or bad, minor or extreme."
+	];
+	data[i++] = [
+			"Unexplained Event",
+			"Something happens. You don't know why."
+	];
+	data[i++] = [
+			"Vivid Detail",
+			"Break into the storyline to describe something in detail so the whole group can share the image. You cannot directly change the course of events, but you might inspire the GM."
+	];
+	data[i++] = [
+			"Bad Company",
+			"Someone's allies turn out to be not so nice after all. Even if their friendship is sincere, you are known by the company you keep."
+	];
+	data[i++] = [
+			"Double or Nothing",
+			"Things are going well. You can stop now, or go for broke. Are you willing to risk it?"
+	];
+	data[i++] = [
+			"Hesitation",
+			"He who hesitates is lost. But how long did he hesitate, and what did he lose?"
+	];
+	data[i++] = [
+			"Just Desserts",
+			"Someone gets what they so richly deserve. It might be good, it might be bad, or it might be ugly."
+	];
+	data[i++] = [
+			"Omens & Portents",
+			"A blood red moon, flocks of crows, and animal entrails all say something big is happening. What is it? Is it good? Is it bad?"
+	];
+	data[i++] = [
+			"Unexplained Phenomenon",
+			"Something happens. You don't know why."
+	];
+
+	dataLength = i;
+	//	displayCard()
+}
